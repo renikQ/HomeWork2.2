617d3e117eefe8c7c3f8f43ad8bd424164c08734
renikQ <trog94@mail.ru>
2022-11-13 19:17:38 +0500

    completed homework 2.2

--- src/Flower.java
+++ src/Flower.java
@@ -1,24 +1,76 @@
 public class Flower {
 
-    private String flowerColor;
+    private String nameFlower;
     private String country;
-    double cost;
-    int lifeSpan;
+    private double cost;
+    private int lifeSpan;
 
-    Flower(String flowerColor, String country, double cost, int lifeSpan) {
-    this.flowerColor = flowerColor;
-    this.country = country;
-    this.cost = cost;
-    this.lifeSpan = lifeSpan;
+    Flower(String nameFlower, String country, double cost, int lifeSpan) {
+        if (nameFlower != null && !nameFlower.isEmpty() && !nameFlower.isBlank()) {
+            this.nameFlower = nameFlower;
+        } else {
+            this.nameFlower = "Роза";
+        }
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+            this.country = country;
+        } else {
+            this.country = "Россия";
+        }
+        this.cost = Math.max(cost, 1);
+        if (lifeSpan > 0) {
+            this.lifeSpan = lifeSpan;
+        } else {
+            this.lifeSpan = 3;
+        }
     }
 
-    public String getFlowerColor() {
-        return flowerColor;
+    public String getNameFlower() {
+        return nameFlower;
+    }
+
+    public void setNameFlower(String nameFlower) {
+        if (nameFlower != null && !nameFlower.isEmpty() && !nameFlower.isBlank()) {
+            this.nameFlower = nameFlower;
+        }
     }
 
     public String getCountry() {
         return country;
     }
 
+    public void setCountry(String country) {
+        if (country != null && !country.isEmpty() && !country.isBlank()) {
+            this.country = country;
+        }
+    }
 
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = Math.max(cost, 1);
+    }
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setLifeSpan(int lifeSpan) {
+        if (lifeSpan > 0) {
+            this.lifeSpan = lifeSpan;
+        } else {
+            this.lifeSpan = 3;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Цветок{" +
+                "Наименование='" + nameFlower + '\'' +
+                ", Страна='" + country + '\'' +
+                ", Цена=" + cost + " руб." +
+                ", срок стояния=" + lifeSpan + " дней" +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        Flower rose = new Flower("Роза обыкновенная", "Голландия", 35.59, 0);
+        Flower chrysanthemum = new Flower("Хризантема", null, 15, 5);
+        Flower pion = new Flower("Пион", "Англия", 69.90, 1);
+        Flower gypsophila = new Flower("Гипсофила", "Турция", 19.50, 10);
+        System.out.println(pion);
+        Bouquet firstBouquet = new Bouquet(new Flower[]{rose, chrysanthemum, pion, gypsophila});
+        System.out.printf("Букет будет стоить %.2f простоит %s дней.", firstBouquet.getCost(), firstBouquet.getLifeSpan());
+
+
 
     }
 }
